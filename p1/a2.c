4a30c5c177dac6c5407c148487363057dc838f24
Gandalf <giovanniapsoliveira@gmail.com>
2014-12-04 00:21:23 -0200

    cheaper soluton for problem 1

---
+++ p1/a2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define LIMIT 1000
+#define A 3
+#define B 5
+#define AB (A*B)
+void main(){
+	int sum = 0, i, n1, n2, n3;
+	n1 = (LIMIT-1)/A;
+	n2 = (LIMIT-1)/B;
+	n3 = (LIMIT-1)/AB;
+	sum += ((A+A*n1)*n1/2);
+	sum += ((B+B*n2)*n2/2);
+	sum -= ((AB+AB*n3)*n3/2);
+	printf("sum: %d\n",sum);
+	return;
+}
