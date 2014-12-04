e9df089ece074c0704d5e7e81ddef474cb577eee
Gandalf <giovanniapsoliveira@gmail.com>
2014-12-03 23:51:19 -0200

    first quick solution

--- p1/a.c
+++ p1/a.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#define LIMIT 1000
 void main(){
-
-}
+	int sum = 0, i;
+	for(i=3;i<LIMIT;i++){
+		if(!(i%3)||!(i%5))
+			sum += i;
+	}
+	printf("sum is: %d\n",sum);
+	return;
+}	
